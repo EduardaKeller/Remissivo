58cf74fda2f6a87a49a7d217c5a7867b8893e9ef
Eduarda <eduarda.skeller@gmail.com>
2020-05-24 20:27:10 -0300

    Modificação

---
+++ src/ListaPalavras.java
@@ -0,0 +1,24 @@
+public class ListaPalavras {
+        private Node header;
+        private Node trailer;
+        private int count;
+
+        private class Node {
+            public Integer element;
+            public Node next;
+            public Node prev;
+            public Node(Integer e) {
+                element = e;
+                next = null;
+                prev = null;
+            }
+        }
+
+        public void DoubleLinkedListOfInteger() {
+            header = new Node(null);
+            trailer = new Node(null);
+            header.next = trailer;
+            trailer.prev = header;
+            count = 0;
+        }
+}
